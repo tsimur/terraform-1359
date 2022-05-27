5bc20712befc40bb4e2bfdb71b3a615ea05bd986
Tsimur Mindarau <7901614@gmail.com>
2022-05-27 19:30:16 +0500

    init

---
+++ first_ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region  = "us-west-2"
+    profile = my_acc
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}

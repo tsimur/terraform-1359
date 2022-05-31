9afabbd612a5669d94810955425dedce7f8d21bc
Tsimur Mindarau <7901614@gmail.com>
2022-05-31 17:18:15 +0500

    add target attachments

--- infra.tf
+++ infra.tf
@@ -12,6 +12,27 @@ resource "aws_vpc" "web_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.web_vpc.id
+
+  tags = {
+    Name = "web_main"
+  }
+}
+
+resource "aws_lb_target_group" "websites_target" {
+  name     = "websites-target"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = aws_vpc.web_vpc.id
+}
+
+resource "aws_route" "route_rule" {
+  route_table_id            = aws_vpc.web_vpc.main_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.gw.id
+}
+
 resource "aws_subnet" "private_subnets" {
   vpc_id                = aws_vpc.web_vpc.id
   cidr_block            = var.private_subnates_ips[count.index]
@@ -50,7 +71,7 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-resource "aws_instance" "website_south" {
+resource "aws_instance" "website" {
   ami           = "ami-09d56f8956ab235b3"
   instance_type = "t2.micro"
   
@@ -63,3 +84,23 @@ resource "aws_instance" "website_south" {
     Name = var.ec2_names[count.index]
   }
 }
+
+resource "aws_lb_target_group_attachment" "web" {
+  target_group_arn = aws_lb_target_group.websites_target.arn
+  target_id        = aws_instance.website[count.index].id
+  port             = 80
+  count = 2
+}
+
+# resource "aws_lb" "test" {
+#   name               = "websites_nlb"
+#   internal           = false
+#   load_balancer_type = "network"
+#   subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+#   enable_deletion_protection = true
+
+#   tags = {
+#     Environment = "production"
+#   }
+# }

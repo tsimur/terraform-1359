1b7debea1942a391180d37220986ce46cf4c6826
Tsimur Mindarau <7901614@gmail.com>
2022-05-31 21:01:13 +0500

    change regioin

--- infra.tf
+++ infra.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region                  = "us-east-1"
+    region                  = "us-east-2"
     shared_credentials_files = ["~/.aws/credentials"]
     profile                 = "my_aws"
 }
@@ -60,8 +60,8 @@ resource "aws_security_group" "allow_tls" {
 
   ingress {
     # TLS (change to whatever ports you need)
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -72,16 +72,32 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_instance" "website" {
-  ami           = "ami-09d56f8956ab235b3"
-  instance_type = "t2.micro"
+  ami                     = "ami-09d56f8956ab235b3"
+  instance_type           = "t2.micro"
   
   vpc_security_group_ids  = [aws_security_group.allow_tls.id]
   subnet_id               = aws_subnet.private_subnets[count.index].id
 
-  count = 2
+  iam_instance_profile    = "AmazonSSMRoleForInstancesQuickSetup"
+
+  count                   = 2
 
   tags = {
     Name = var.ec2_names[count.index]
+  }
+}
+
+resource "aws_lb" "web_balancer" {
+  name               = "websites-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  # subnets            = [for subnet in aws_subnet.private_subnets : subnet.id]
+  subnets            = [aws_subnet.private_subnets[0].id, aws_subnet.private_subnets[1].id]
+
+  enable_deletion_protection = true
+
+  tags = {
+    Environment = "two_websites"
   }
 }
 
@@ -92,15 +108,13 @@ resource "aws_lb_target_group_attachment" "web" {
   count = 2
 }
 
-resource "aws_lb" "test" {
-  name               = "websites_nlb"
-  internal           = false
-  load_balancer_type = "network"
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+resource "aws_lb_listener" "WebSites" {
+  load_balancer_arn = aws_lb.web_balancer.arn
+  port              = "80"
+  protocol          = "TCP"
 
-  enable_deletion_protection = true
-
-  tags = {
-    Environment = "production"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.websites_target.arn
   }
 }

30bded3a0bef045714580a6ec968de3075e0a1c9
Tsimur Mindarau <7901614@gmail.com>
2022-06-06 15:55:07 +0500

    ready to split main file

--- sc.sh
+++ sc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo systemctl restart nginx
-sudo systemctl enable nginx
+# sudo apt-get -y install nginx
+# sudo systemctl restart nginx
+# sudo systemctl enable nginx
+sudo apt-get -y install php
+cat <<\EOT >> /tmp/index.php
+<?php
+$localIP = getHostByName(getHostName());
+echo "<h1>Local IP is:</h1>";
+echo "<h1 style='color:red; font-size: 100px;'>".$localIP."</h1>";
+?>
+EOT
+sudo cp /tmp/index.php /var/www/html
+sudo rm /var/www/html/index.html
